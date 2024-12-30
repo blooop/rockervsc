312f6f4c1d1de7827d32d36a062615fccdf3d937
Austin Gregg-Smith <blooop@gmail.com>
2024-12-30 15:07:09 +0000

    force the container name to be lowercase to match docker naming convention

--- rockervsc/rockervsc.py
+++ rockervsc/rockervsc.py
@@ -20,7 +20,8 @@ def folder_to_vscode_container(container_name: str, path: Path) -> Tuple[str, st
     """
 
     container_hex = binascii.hexlify(container_name.encode()).decode()
-    rocker_args = f"--image-name {container_name} --name {container_name} --volume {path}:/workspaces/{container_name}:Z --oyr-run-arg '\" --detach\"'"
+    container_name_lower = container_name.lower()
+    rocker_args = f"--image-name {container_name} --name {container_name_lower} --volume {path}:/workspaces/{container_name_lower}:Z --oyr-run-arg '\" --detach\"'"
 
     return container_hex, rocker_args
 
